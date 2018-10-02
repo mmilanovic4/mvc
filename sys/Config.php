eeee50406dade2dd6db2f5656529aa15c2309782
Miloš Milanović <mmilanovic016@gmail.com>
2018-10-02 05:46:19 +0200

    Add PHPDoc comments in configuration file

--- sys/Config.php
+++ sys/Config.php
@@ -5,15 +5,51 @@
  */
 final class Config {
 
+	/**
+	 * Апсолутни линк апликације
+	 * @var string
+	 */
 	const BASE = 'http://localhost/dev/MVC/';
+
+	/**
+	 * Релативни линк апликације (на продукцији најчешће само '/')
+	 */
 	const PATH = '/dev/MVC/';
 
+	/**
+	 * Сервер БП: име хоста
+	 * @var string
+	 */
 	const DB_HOST = 'localhost';
+
+	/**
+	 * Сервер БП: корисничко име
+	 * @var string
+	 */
 	const DB_USER = 'root';
+
+	/**
+	 * Сервер БП: лозинка
+	 * @var string
+	 */
 	const DB_PASS = '';
+
+	/**
+	 * Сервер БП: име базе
+	 * @var string
+	 */
 	const DB_NAME = 'mvc';
 
+	/**
+	 * Сесијска променљива у којој ће бити складиштен корисников ИД приликом пријаве
+	 * @var string
+	 */
 	const USER_COOKIE = 'user_id';
+
+	/**
+	 * Случајни или псеудо-случајни стринг произвољне дужине
+	 * @var string
+	 */
 	const SALT = '34aa3fb2c440cac0b1cdbb49146a2f34';
 
 }
--- sys/autoload.php
+++ sys/autoload.php
@@ -2,24 +2,31 @@
 
 /**
  * Кастомизована autoload функција
+ * @return bool
  */
 spl_autoload_register(function($className) {
 	if (file_exists('./sys/classes/' . $className . '.php')) {
 		// Укључивање класа из sys/classes фолдера
 		require_once './sys/classes/' . $className . '.php';
+		return true;
 	} elseif (preg_match('|^(?:[A-Z][a-z]+)+Controller$|', $className)) {
 		// Укључивање контролера
 		require_once './app/controllers/' . $className . '.php';
+		return true;
 	} elseif (preg_match('|^(?:[A-Z][a-z]+)+Model$|', $className)) {
 		// Укључивање модела
 		require_once './app/models/' . $className . '.php';
+		return true;
 	} elseif ($className === 'Config') {
 		// Укључивање конфигурационог фајла
 		if (file_exists('./sys/Config.php')) {
 			require_once './sys/Config.php';
+			return true;
 		} else {
 			ob_clean();
 			die('Configuration error - Config file not found!');
 		}
 	}
+	// Класа није пронађена
+	return false;
 });
