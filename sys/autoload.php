d43b04d4cced1bc6b3c280919f6addab40d52828
Miloš Milanović <mmilanovic016@gmail.com>
2018-11-22 20:58:31 +0100

    Improve autoloader - more verbose messages

--- sys/autoload.php
+++ sys/autoload.php
@@ -5,28 +5,29 @@
  * @return bool
  */
 spl_autoload_register(function($className) {
+	$path = '';
+
 	if (file_exists('./sys/classes/' . $className . '.php')) {
 		// Укључивање класа из sys/classes фолдера
-		require_once './sys/classes/' . $className . '.php';
-		return true;
+		$path = './sys/classes/' . $className . '.php';
 	} elseif (preg_match('|^(?:[A-Z][a-z]+)+Controller$|', $className)) {
 		// Укључивање контролера
-		require_once './app/controllers/' . $className . '.php';
-		return true;
+		$path = './app/controllers/' . $className . '.php';
 	} elseif (preg_match('|^(?:[A-Z][a-z]+)+Model$|', $className)) {
 		// Укључивање модела
-		require_once './app/models/' . $className . '.php';
-		return true;
+		$path = './app/models/' . $className . '.php';
 	} elseif ($className === 'Config') {
 		// Укључивање конфигурационог фајла
-		if (file_exists('./sys/Config.php')) {
-			require_once './sys/Config.php';
-			return true;
-		} else {
-			ob_clean();
-			die('AUTOLOAD: Configuration file not found.');
-		}
+		$path = './sys/Config.php';
+	} else {
+		die('AUTOLOAD: Class not found.');
 	}
+
+	if (file_exists($path)) {
+		require_once $path;
+		return true;
+	}
+
 	// Класа није пронађена
-	return false;
+	die('AUTOLOAD: File not found.');
 });
